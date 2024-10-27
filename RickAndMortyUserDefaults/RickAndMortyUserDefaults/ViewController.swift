46ebb12ce9347d9a565be75c6ce31ff856638913
ronesta <gvozzzd123@gmail.com>
2024-10-27 17:30:11 +0300

    Initial commit

---
+++ RickAndMortyUserDefaults/RickAndMortyUserDefaults/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  RickAndMortyUserDefaults
+//
+//  Created by Ибрагим Габибли on 27.10.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+}
+
